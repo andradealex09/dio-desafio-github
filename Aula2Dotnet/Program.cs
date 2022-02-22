efa1212d9f876d7b7ad692c3a30d9b887116e6a9
Alex Andrade <andradealex09@gmail.com>
2022-02-21 21:56:11 -0300

    Subindo Aula2Dotnet

---
+++ Aula2Dotnet/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+public class Program
+{
+    static void Demo6()
+    {
+        int[] numeros = new int[] {0,2,4,6,8};
+      WriteLine($"Digite o número que gostariade encontrar");
+      var numero = int.Parse(ReadLine());
+      var idxEncontrado = EncontrarNumero(numeros, numero);
+
+        if(idxEncontrado >= 0)
+        {
+            WriteLine($"O número digitado está na posição {idxEncontrado}");
+        }
+        else
+        {
+            WriteLine("O número digitado não foi encontrado");
+        }
+    }
+    static void Demo5()
+    {
+         int[] pares = new int[]{0,2,4,6,8};
+
+       MudarParaImpar(pares);
+
+       WriteLine($"Os imparares {string.Join(",", pares)} ");
+
+    }
+    static void Demo4()
+    {
+        string nome = "Alex";
+
+        TrocarNome(nome, "Andrade");
+        
+        WriteLine($"O novo nome é {nome}");
+    }
+    static void Demo3()
+    {
+        StructPessoa p1 = new StructPessoa()
+       {
+           Documento ="1234",
+           Idade = 30,
+           Nome = "Alex"
+       };
+
+       var p2 = p1; 
+
+       p1 = TrocarNome(p1, "Andrade");
+
+       WriteLine($@"
+       Nome do p1: {p1.Nome}
+       Nome do p2: {p2.Nome}
+       ");
+    }
+    static void Demo2()
+    {
+        
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Alex Andrade";
+        p1.Idade = 28;
+        p1.Documento = "123450";
+
+        Pessoa p2 = p1.Clone();
+
+
+        TrocarNome(p1, "Andrade");
+
+
+
+        WriteLine($@"
+        O nome de p1 é: {p1.Nome}
+        O nome de p2 é: {p2.Nome}
+        ");
+
+    }
+    static void Demo1()
+    {
+        int a = 2;
+        a = Adcionar20(a);
+        WriteLine($"O valor da variável a é {a}");
+    }
+    
+    static int Adcionar20(int x)
+    {
+     return x + 20;
+    }
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+   
+    static StructPessoa TrocarNome(StructPessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+        return p1;
+    }
+    static void TrocarNome(string nome, string nomeNovo)
+    {
+        nome = nomeNovo;
+    }
+    static void MudarParaImpar(int[]pares)
+    {
+        for (int i = 0; i < pares.Length; i++)
+        {
+            pares [i] = pares[i] + 1;
+        }
+    }
+
+    static int EncontrarNumero(int[]numeros, int numero) 
+    {
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if(numeros[i] == numero)
+                return i;
+
+        }
+        return -1;
+    }
+
+    static bool EncontrarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+    {
+        foreach (var item in pessoas)
+        {
+            if(item.Nome == pessoa.Nome)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void Main()
+    {  
+       
+      List<Pessoa> pessoas = new List<Pessoa>()
+      {
+          new Pessoa(){Nome = "Alex"},
+          new Pessoa(){Nome = "Luziano"},
+          new Pessoa(){Nome = "ALice"},
+          new Pessoa(){Nome = "Guilherme"},
+          new Pessoa(){Nome = "Andrezza"},
+          
+      };
+
+      WriteLine("Digite a pessoa que gostaria de localizar:");
+      var nome = ReadLine();
+      var pessoa = new Pessoa(){Nome = nome};
+      var encontrado = EncontrarPessoa(pessoas, pessoa);
+      if(encontrado)
+      {
+          WriteLine("Pessoa Localizada");
+      }
+      else
+      {
+          WriteLine("Pessoas não encontrada");
+      }
+
+        
+    } 
+}
